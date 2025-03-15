c7d09ff42c37fdffe00c7c78c5b461109291e0d7
Khemraj <khemraj@tutopiya.comgit>
2025-03-15 14:41:29 +0530

    Update the site config file for site syncing eith DB.

--- wp-config.php
+++ wp-config.php
@@ -21,16 +21,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'local' );
+define( 'DB_NAME', 'icei_38523038_local' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'icei_38523038' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_PASSWORD', 'PAiKLe7DSH2l' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'sql304.iceiy.com' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
